60ca6fca381cbd7f0fabdc9105a8265b8c1c226b
vVuc <tmenezessilva34@gmail.com>
2026-01-30 15:00:23 -0300

    first commit

---
+++ src/main/java/com/nology/leaddecisions/LeadDecisionsApplication.java
@@ -0,0 +1,13 @@
+package com.nology.leaddecisions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadDecisionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeadDecisionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nology/leaddecisions/LeadDecisionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.leaddecisions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadDecisionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
